8bf6a27da3623af74bd09021dcf5278d69eaa158
Torocsik_Daniel_Zoltan_ <danaharcos25@gmail.com>
2026-05-13 10:14:28 +0200

    hatter

--- KozosRajz.py
+++ KozosRajz.py
@@ -2,3 +2,23 @@
 #T_Daniel hatter
 #T_Daniel oszlop
 
+#hatter
+import turtle
+turtle.speed(0)
+
+turtle.color("lightblue")
+turtle.penup()
+turtle.goto(-300,300)
+turtle.pendown()
+turtle.fillcolor("lightblue")
+turtle.begin_fill()
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(600)
+turtle.end_fill()
+
+turtle.done()
